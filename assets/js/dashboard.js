39a339f29c8d90d731daa4dbd40fa71d3091a04f
Marco De Laet <delaet.marco@gmail.com>
2022-05-23 20:49:17 -0300

    showing message for 0 records on dashboard

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -63,58 +63,65 @@ function handleListOnLoad(uid,status,month,year,search) {
             if (this.readyState == 4 && this.status == 200) {
                 // Typical action to be performed when the document is ready:
                 obj         = JSON.parse(request.responseText);
-                html        = '';
-                amount_total= 0;
-                for(var i=0;i < obj.length; i++){
-                    var formatter = new Intl.NumberFormat(lang, {
-                        style: 'currency',
-                        currency: obj[i].currency,
-                        //maximumSignificantDigits: 2,
-                
-                        // These options are needed to round to whole numbers if that's what you want.
-                        //minimumFractionDigits: 2, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-                        //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
-                    });
-                    color_status = '#d60b0e';
-                    if(obj[i].status_percent == '100')
-                        color_status = '#298c3d';
-                    if(obj[i].status_percent == '90')
-                        color_status = '#03fc84';
-                    if(obj[i].status_percent == '75')
-                        color_status = '#77fc03';
-                    if(obj[i].status_percent == '50')
-                        color_status = '#ebfc03';
-                    if(obj[i].status_percent == '25')
-                        color_status = '#fc3d03';
-                        
-
-                    agency = '';
-                    if(typeof(obj[i].agency_name) === 'string')
-                        agency = ' / '+obj[i].agency_name;
-                    amount = obj[i].amount_int / 100;
-                    amount_month = obj[i].amount_per_month_int / 100;
-                    amount_total+= parseInt(obj[i].amount_per_month_int);
-                    amount_total_show   = amount_total / 100;
-                    percentGoalShow     = (100 * amount_total)/parseInt(document.getElementById('goal-2').innerText);
-                    start_date  = new Date(obj[i].start_date);
-                    stop_date   = new Date(obj[i].stop_date);
-                    html += '<tr><td>'+obj[i].offer_name+'</td><td nowrap>'+obj[i].client_name+agency+'</td><td nowrap>'+obj[i].username+'</td><td nowrap><spam style="display:none;" class="currency-line" id="currency-'+(i)+'">'+obj[i].currency+'</spam><spam class="amount-line" id="amount-'+(i)+'">'+formatter.format(amount)+'</spam></td><td nowrap><spam class="amount-month-line" id="amount-month-'+(i)+'">'+formatter.format(amount_month)+'</spam></td><td style="text-align:center;"><span id="locked_status_'+obj[i].UUID+'" class="material-icons" title="'+obj[i].status_percent+'% '+obj[i].status_name+'" style="color:'+color_status+'">thermostat</span></td><td nowrap style="text-align:center;">';
-                    // information card
-                    html += '<a href="?pr=Li9wYWdlcy9wcm9wb3NhbHMvaW5mby5waHA=&tid='+obj[i].UUID+'"><span class="material-icons" style="font-size:1.5rem; color:black;" title="Information Card '+obj[i].offer_name+'">info</span></a>';
-
-                    // Edit form
-                    html += '<a href="?pr=Li9wYWdlcy9wcm9wb3NhbHMvZm9ybWVkaXQucGhw&tid='+obj[i].UUID+'"><span class="material-icons" style="font-size:1.5rem; color:black;" title="Edit '+module_item + ' '+obj[i].offer_name+'">edit</span></a>';
-
-                    // Remove 
-                    html += '<a href="javascript:void(0)" onclick="handleRemove(\''+obj[i].UUID+'\',\''+obj[i].is_active+'\')"><span class="material-icons" style="font-size:1.5rem; color:black;" title="Remove '+module_item + ' '+obj[i].offer_name+'">delete</span></a>';
-
+                if(typeof obj[0].response != 'undefined'){
+                    html = '<tr><td colspan="8"><div style="margin-left:30%; margin-right:30%;text-align:center;" role="status">';
+                    html += '0 proposals';
                     html += '</td></tr>';
+                    tableList.innerHTML = html;    
+                    }else{
+                    html        = '';
+                    amount_total= 0;
+                    for(var i=0;i < obj.length; i++){
+                        var formatter = new Intl.NumberFormat(lang, {
+                            style: 'currency',
+                            currency: obj[i].currency,
+                            //maximumSignificantDigits: 2,
+                    
+                            // These options are needed to round to whole numbers if that's what you want.
+                            //minimumFractionDigits: 2, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+                            //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+                        });
+                        color_status = '#d60b0e';
+                        if(obj[i].status_percent == '100')
+                            color_status = '#298c3d';
+                        if(obj[i].status_percent == '90')
+                            color_status = '#03fc84';
+                        if(obj[i].status_percent == '75')
+                            color_status = '#77fc03';
+                        if(obj[i].status_percent == '50')
+                            color_status = '#ebfc03';
+                        if(obj[i].status_percent == '25')
+                            color_status = '#fc3d03';
+                            
+
+                        agency = '';
+                        if(typeof(obj[i].agency_name) === 'string')
+                            agency = ' / '+obj[i].agency_name;
+                        amount = obj[i].amount_int / 100;
+                        amount_month = obj[i].amount_per_month_int / 100;
+                        amount_total+= parseInt(obj[i].amount_per_month_int);
+                        amount_total_show   = amount_total / 100;
+                        percentGoalShow     = (100 * amount_total)/parseInt(document.getElementById('goal-2').innerText);
+                        start_date  = new Date(obj[i].start_date);
+                        stop_date   = new Date(obj[i].stop_date);
+                        html += '<tr><td>'+obj[i].offer_name+'</td><td nowrap>'+obj[i].client_name+agency+'</td><td nowrap>'+obj[i].username+'</td><td nowrap><spam style="display:none;" class="currency-line" id="currency-'+(i)+'">'+obj[i].currency+'</spam><spam class="amount-line" id="amount-'+(i)+'">'+formatter.format(amount)+'</spam></td><td nowrap><spam class="amount-month-line" id="amount-month-'+(i)+'">'+formatter.format(amount_month)+'</spam></td><td style="text-align:center;"><span id="locked_status_'+obj[i].UUID+'" class="material-icons" title="'+obj[i].status_percent+'% '+obj[i].status_name+'" style="color:'+color_status+'">thermostat</span></td><td nowrap style="text-align:center;">';
+                        // information card
+                        html += '<a href="?pr=Li9wYWdlcy9wcm9wb3NhbHMvaW5mby5waHA=&tid='+obj[i].UUID+'"><span class="material-icons" style="font-size:1.5rem; color:black;" title="Information Card '+obj[i].offer_name+'">info</span></a>';
+
+                        // Edit form
+                        html += '<a href="?pr=Li9wYWdlcy9wcm9wb3NhbHMvZm9ybWVkaXQucGhw&tid='+obj[i].UUID+'"><span class="material-icons" style="font-size:1.5rem; color:black;" title="Edit '+module_item + ' '+obj[i].offer_name+'">edit</span></a>';
+
+                        // Remove 
+                        html += '<a href="javascript:void(0)" onclick="handleRemove(\''+obj[i].UUID+'\',\''+obj[i].is_active+'\')"><span class="material-icons" style="font-size:1.5rem; color:black;" title="Remove '+module_item + ' '+obj[i].offer_name+'">delete</span></a>';
+
+                        html += '</td></tr>';
+                    }
+                    tableList.innerHTML = html;
+                    document.getElementById('goal-1').innerHTML = formatter.format(amount_total_show);
+                    document.getElementById('goal-percent').innerHTML = percentGoalShow.toFixed(2) + "%";
                 }
-                tableList.innerHTML = html;
-                document.getElementById('goal-1').innerHTML = formatter.format(amount_total_show);
-                document.getElementById('goal-percent').innerHTML = percentGoalShow.toFixed(2) + "%";
             }
-            else{
+            else {
                 html = '<tr><td colspan="8"><div style="margin-left:45%; margin-right:45%;" class="spinner-border" style="text-align:center;" role="status">';
                 html += '<span class="sr-only">Loading...</span>';
                 html += '</div></td></tr>';
