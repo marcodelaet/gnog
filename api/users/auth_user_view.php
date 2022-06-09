8071861523c459e524503b7295c61c0dae93910c
Marco De Laet <delaet.marco@gmail.com>
2022-06-09 20:38:24 -0300

    backing user_list_view file to original set

--- api/users/auth_user_view.php
+++ api/users/auth_user_view.php
@@ -14,54 +14,46 @@ $con = $DB->connect();
 if(array_key_exists('auth_api',$_REQUEST)){
     // check auth_api === local storage
     //if($localStorage == $_REQUEST['auth_api']){}
-    $sec = 'offlll';
-    if(array_key_exists('lacc',$_COOKIE))
-        $sec = $_COOKIE['lacc'];
-    if($sec >= 99999){
-        // filters
-        $uuid                   = '';
-        $username               = ''; 
-        $email                  = '';
-        $mobile                 = '00000000000';
 
-        $filters                = '';
-        if(array_key_exists('search',$_REQUEST)){
-            if($_REQUEST['search']!==''){
-                if($filters != '')
-                    $filters .= " AND ";
-                $jocker         = $_REQUEST['search'];
-                $filters        .= " search like '%$jocker%'";
-            }
+    // filters
+    $uuid                   = '';
+    $username               = ''; 
+    $email                  = '';
+    $mobile                 = '00000000000';
+
+    $filters                = '';
+    if(array_key_exists('search',$_REQUEST)){
+        if($_REQUEST['search']!==''){
+            if($filters != '')
+                $filters .= " AND ";
+            $jocker         = $_REQUEST['search'];
+            $filters        .= " search like '%$jocker%'";
         }
-        if(array_key_exists('mobile',$_REQUEST)){
-            if($_REQUEST['mobile']!==''){
-                if($filters != '')
-                    $filters .= " AND ";
-                $mobile         = $_REQUEST['mobile'];
-                $filters        .= " mobile like '%$mobile%'";
-            }
-        }
-        if(array_key_exists('email',$_REQUEST)){
-            if($_REQUEST['email']!==''){
-                if($filters != '')
-                    $filters .= " AND ";
-                $email         = $_REQUEST['email'];
-                $filters        .= " email like '%$email%'";
-            }
-        }
-        if(array_key_exists('username',$_REQUEST)){
-            if($_REQUEST['username'] !==''){
-                if($filters != '')
-                    $filters .= " AND ";
-                $username         = $_REQUEST['username'];
-                $filters        .= " username like '%$username%'";
-            }
-        }
-    } else {
-        if($sec != 'offlll')
-            $filters = "uuid = '".$_COOKIE['uuid']."'";
     }
-
+    if(array_key_exists('mobile',$_REQUEST)){
+        if($_REQUEST['mobile']!==''){
+            if($filters != '')
+                $filters .= " AND ";
+            $mobile         = $_REQUEST['mobile'];
+            $filters        .= " mobile like '%$mobile%'";
+        }
+    }
+    if(array_key_exists('email',$_REQUEST)){
+        if($_REQUEST['email']!==''){
+            if($filters != '')
+                $filters .= " AND ";
+            $email         = $_REQUEST['email'];
+            $filters        .= " email like '%$email%'";
+        }
+    }
+    if(array_key_exists('username',$_REQUEST)){
+        if($_REQUEST['username']!==''){
+            if($filters != '')
+                $filters .= " AND ";
+            $username         = $_REQUEST['username'];
+            $filters        .= " username like '%$username%'";
+        }
+    }
     if($filters !== ''){
         $filters = "WHERE ".$filters;
     }
@@ -70,7 +62,7 @@ if(array_key_exists('auth_api',$_REQUEST)){
     // Query creation
     $sql = "SELECT left(uuid,13)as uuid, uuid as uuid_full, level_account, username, email, mobile_number,mobile_international_code, concat('+',mobile_international_code,mobile_number) as mobile, account_locked FROM view_users $filters order by username";
     // LIST data
-    echo $sql;
+    //echo $sql;
     $rs = $DB->getData($sql);
 
   
